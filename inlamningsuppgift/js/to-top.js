1e3cf60ad73bd38b7155e093763538c8082c6f29
Duker <m.oliver.henriksson@gmail.com>
2023-09-19 00:37:01 +0200

    Optimization didn't really optimize so gone it goes. Also added to-top button to contact

--- inlamningsuppgift/js/to-top.js
+++ inlamningsuppgift/js/to-top.js
@@ -1,12 +1,11 @@
 window.addEventListener('scroll',
    function () {
-      var elmnt = this.document.getElementById("to-top");
 
-      if (elmnt.style.display != "block" && window.scrollY > 400) {
-         elmnt.style.display = "block";
+      if (window.scrollY > 400) {
+         this.document.getElementById("to-top").style.display = "block";
 
-      } else if (elmnt.style.display != "none" && window.scrollY < 400) {
-         elmnt.style.display = "none";
+      } else {
+         this.document.getElementById("to-top").style.display = "none";
       }
    }
 );
